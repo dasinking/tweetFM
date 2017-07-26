26e487903e65b2e0c37cdafdee8b99b75d174290
Alexander L <alexander@dasinking.de>
2017-07-26 22:30:28 +0200

    v4-a2
    in a nutshell:
    
    - much more stable
    - oAuth
    - more rescues
    
    for more: see changelog.txt

--- README.md
+++ README.md
@@ -3,12 +3,22 @@ A ruby script for tweeting your latest scrobbles
 
 ## Requirements
 
-* A Twitter and Last.FM-account (obviously)
-* Ruby 2.0+
-* the following gems: yaml, rubygems, json, twitter, lastfm, logger
+* A Twitter and last.fm-account (obviously)
+* Ruby 2.0+ (tested with 2.3.0)
+* the following gems: yaml, rubygems, twitter, lastfm, oauth
 
 ## Usage
 
-1. Copy `config.yml.example` to `config.yml` and modify it to your needs.
-2. Run the tweetFM.rb
-3. Well, that's it. It's that simple.
+1. Run tweetFM.rb
+2. Well, that's it. It's that simple.
+
+## sidenotes
+
+If you have problems, check whether or not the config.yml looks similar to the config.yml.example.
+You still have problems? Shoot me a tweet @dasinking.
+
+If you experience any crashes, please include the error message from the command line.
+
+And no, I don't save any of your data. You use two applications of mine to access the API from Twitter and last.fm. I can't access any of your files, as the auth info is saved in your config.yml, so your precious memes are safe.
+
+Also huge thanks to @nilsding for helping. 
--- tweetFM.rb
+++ tweetFM.rb
@@ -1,98 +1,194 @@
-# dHdlZXRGTV92NCBieSBkYXNpbmtpbmcgLSB3aXRoIGxvdmUgdG8gbXVzaWMgYW5kIEUu
-version = "tweetFM_v4-alpha1 by @dasinking"
-
+# dHdlZXRGTSB2NCBieSBkYXNpbmtpbmcgLSBtYWRlIGluIDIwMTYtMTcgd2l0aCA8Mw==
+@version = "tweetFM_v4-alpha2"
 require 'yaml'
 require 'rubygems'
 require 'twitter'
 require 'lastfm'
-require 'logger'
+require 'oauth'
 
-#config_load
-@CONFIG = YAML.load_file File.expand_path '../config.yml', __FILE__
-@log = Logger.new('log.txt', 'daily')
-#@log.debug (version + ' started')
-@log.debug("#{version} started")
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE                       #only needed for some ruby installations on windows, when you get a certificate error (fuck that)
 
-#Twitter_auth
-def twitauth
-	@Twitter = Twitter::REST::Client.new do |config|
-	  config.consumer_key    		= @CONFIG[:twitter_key]
-	  config.consumer_secret 		= @CONFIG[:twitter_secret]
-	  config.access_token        	= @CONFIG[:twitter_token]
-	  config.access_token_secret 	= @CONFIG[:twitter_token_secret]
-	end
-	@log.debug 'Twitter logged in'
+#####twitter_auth#####
+  @Twitter = Twitter::REST::Client.new do |config|
+    config.consumer_key       = "4iTHA7VaA85qX0gaIT6fskn9f"
+    config.consumer_secret    = "3GQIVok9vUqob30DfQlghKLM7oACHqKaJtI1YMi90LfKI7a1KT"
+  end
+#####twitter_auth_end#####
+
+#####lastfm_auth#####
+  @lastfm = Lastfm.new("d40320e36eca2707150f55723630436e", "aff6f8a58d4ad1a937e0128a83dec486")
+#####lastfm_auth_end#####
+
+def setup
+  if File.file?("config.yml") == true then                                  #check for existing config 
+    $config = {
+
+    }.merge(YAML.load_file File.expand_path '../config.yml', __FILE__)      #it's already there? yay. no setup required.
+
+    if $config[:revision] != 1 then                                         #search for revision. this procedure is futureproof by increasing the number! so smart!!
+      begin
+        puts ""
+        puts "Welcome to #{@version}! Looks like your update was successful, but we're not done yet."
+        puts "Your existing configuration is based on an older installation of tweetFM."
+        puts "A new configuration will be created, but before that, we need to delete the old one."
+        puts "If you have any emotional connection to it, create a backup now."
+        puts "When you're done, or you don't care, press enter."
+        stub = $stdin.gets.strip                                              #wait for enter press
+        File.delete("config.yml")                                             #delete old config
+        $config = {}                                                          #zero the variable
+        setup                                                                 #rerun setup for configuring the new config file
+      end                                                                                      #yo dawg, i heard you like configurations
+    else                                                                      #up-to-date config is found and configurations will be set
+      begin
+        @Twitter.access_token = $config[:twitter_token]
+        @Twitter.access_token_secret = $config[:twitter_token_secret]
+        @lastfmuser = $config[:lastfm_user]
+        if $config[:url] == true then                                         #resetting the url with every start of tweetFM, heheheh
+          @Twitter.update_profile(:url => "https://github.com/dasinking/tweetFM/")
+        end
+      end
+    end
+  else                                                                        #o fuc
+    puts ""
+    puts "Welcome, new user of tweetFM!"
+    puts "Before we can start spamming you and your friends' timeline, we need to configure some things."
+    puts "It won't take long!"
+
+    $config = {}
+
+    #####twitter_auth#####
+      puts ""
+      puts "First we need access to Twitter."
+      consumer = OAuth::Consumer.new(@Twitter.consumer_key,@Twitter.consumer_secret, :site => "https://api.twitter.com")  #define where to send the request and it's parameters
+      request_token = consumer.get_request_token(:oauth_callback => @callback_url)                                        #requesting and saving first oauth answer
+
+      url = request_token.authorize_url(oauth_callback: @callback_url)                                                    #saving the auth url separately 
+
+      puts "Open this URL in a browser: #{url}"
+      pin = ''                                    #init pin variable (zeroing)
+      until pin =~ /^\d+$/                        #loop for typing in the auth pin
+        print "Enter PIN => "
+        pin = $stdin.gets.strip                   #saving pin
+      end
+
+      @access_token = request_token.get_access_token(oauth_verifier: pin)   #requesting the tokens with the auth pin and saving it
+
+      $config[:twitter_token] = @access_token.token                         #defining the tokens in the config
+      $config[:twitter_token_secret] = @access_token.secret                 #like that, we need to reload setup in the end, so it defines the tokens
+
+      if @Twitter.user("dasinking").id == 78006580 then                     #test whether or not i can access the api
+        puts ""
+        puts "Successfully logged into Twitter!"
+      else 
+        puts ""
+        puts "somethings wrong with the auth process wtf"
+        puts "that should be impossible actually"
+        auth
+      end
+    #####twitter_auth_end#####
+
+    #####lastfm username#####
+      puts ""
+      puts "Now, enter your last.fm-username:"
+      username = ''                                     #init username variable (zeroing)
+      print "Username => "
+      username = $stdin.gets.strip                      #saving username
+      $config[:lastfm_user] = username
+    #####lastfm username_end#####
+
+    #####source code link y/n#####
+      puts
+      puts "tweetFM is free and open source, so it would be really nice of you to link the GitHub repository in your profile."
+      puts "Do you want me to do this for you? (y/n)"
+      link = ''                                     #init username variable (zeroing)
+
+      print ""
+      link = $stdin.gets.strip                      #saving username
+      
+      if link == "n" then                           #when they type anything else than no it'll change the url #dickmove
+        $config[:url] = false  
+      else
+        $config[:url] = true
+      end
+    #####source code link y/n_end#####
+
+    #####create the config file#####
+      $config[:revision] = 1
+      File.open "config.yml", 'w' do |f|              #save contents of the config hash 
+        f.write $config.to_yaml                       #... in yaml
+      end
+      setup                                           #rerun setup for setting the parameters
+    #####create the config file_end#####
+  end
 end
 
 def tweet(artist, track)
-	if "#{artist}#{track}".size < 123 then
-		#@tweetoutput = '♫ ' + artist + ' - ' + track + ' #NowPlaying'
-		@tweetoutput = "♫ #{artist} - #{track} #NowPlaying"
-		@Twitter.update(@tweetoutput)
-		#puts '[' + Time.new.strftime("%d-%m-%Y %H:%M:%S").to_s + '] ' + 'Tweet sent: ' + @tweetoutput
-		puts "[#{Time.new.strftime("%d-%m-%Y %H:%M:%S").to_s}] Tweet sent: #{@tweetoutput}"
-	else
-		artistlength = artist.size
-		chomper = 123 - artistlength - 3
-		#@tweetoutput = '♫ ' + artist + ' - ' + track[0...chomper] + '...' + ' #NowPlaying'
-		@tweetoutput = "♫ #{artist} - #{track[0...chomper]}... #NowPlaying"
-		@Twitter.update(@tweetoutput)
-		#puts '[' + Time.new.strftime("%d-%m-%Y %H:%M:%S").to_s + '] ' + 'Tweet sent: ' + @tweetoutput		
-		puts "[#{Time.new.strftime("%d-%m-%Y %H:%M:%S").to_s}] Tweet sent: #{@tweetoutput}"
-	end
+  if "#{artist}#{track}".size < 123 then
+    begin
+      @tweetoutput = "♫ #{artist} - #{track} #NowPlaying"
+      @Twitter.update(@tweetoutput)
+      puts "[#{Time.new.strftime("%d-%m-%Y %H:%M:%S").to_s}] Tweet sent: #{@tweetoutput}"
+    rescue => e 
+      puts "rescued (probably twitter is down other some shit): " + e.message
+      sleep(3)
+      retry while true
+    end
+  else
+    begin
+      artistlength = artist.size
+      chomper = 123 - artistlength - 3
+      @tweetoutput = "♫ #{artist} - #{track[0...chomper]}... #NowPlaying"
+      @Twitter.update(@tweetoutput)
+      puts "[#{Time.new.strftime("%d-%m-%Y %H:%M:%S").to_s}] Tweet sent: #{@tweetoutput}"      
+    rescue => e 
+      puts "rescued (probably twitter is down other some shit): " + e.message
+      sleep(3)
+      retry while true
+    end
+  end
 end
 
-#Lastfm_auth
-@lastfm = Lastfm.new(@CONFIG[:lastfm_apikey], @CONFIG[:lastfm_apisecret])
-@log.debug 'Lastfm logged in'
+def scrobbleread
+  sleep(1)
+  @recent = @lastfm.user.get_recent_tracks(user: @lastfmuser, limit: 1)      #get latest scrobbles from lastfm's api
+  rescue => e                                                                #rescue when down
+  puts 'LastFM-API down: ' + e.message
+  retry while true
+end
 
 #MAINLOOP
 begin
-	twitauth
-	#@Twitter.update('[' + Time.new.strftime("%d-%m-%Y %H:%M:%S").to_s + '] ' + version + ' == online')
-	@Twitter.update("[#{Time.new.strftime("%d-%m-%Y %H:%M:%S").to_s}] #{version} == online")
-	#puts('[' + Time.new.strftime("%d-%m-%Y %H:%M:%S").to_s + '] ' + version + ' == online')
-	puts "[#{Time.new.strftime("%d-%m-%Y %H:%M:%S").to_s}] #{version} == online"
+    setup
 
-	while 1 != 2 do
-		begin
-			twitauth
-			#SCROBBLEREAD
-			begin 
-				sleep(1)
-				@recent = @lastfm.user.get_recent_tracks(user: @CONFIG[:lastfm_user], api_key: @lastfm_apikey, limit: 1)
-				rescue => e
-				@log.error ('LastFM-API down')
-				retry while true
-			end
+    @Twitter.update("[#{Time.new.strftime("%d-%m-%Y %H:%M:%S").to_s}] #{@version} by @dasinking == online")  #welcome online
+    puts "[#{Time.new.strftime("%d-%m-%Y %H:%M:%S").to_s}] #{@version} by @dasinking == online"
 
-			#tweet if no active scrobbling
-			if @recent.is_a? Hash
-				$date1 = @recent["date"]["uts"]
-				if $date1 != $date2
-					begin
-						tweet(@recent["artist"]["content"],@recent["name"])
-						#@log.info ('Tweet sent: ♫ ' + @recent["artist"]["content"] + ' - ' + @recent["name"] + ' #NowPlaying')
-						@log.info("Tweet sent: ♫ #{@recent["artist"]["content"]} - #{@recent["name"]} #NowPlaying")
-						$date2 = $date1
-					end
-				end	
-			#tweet if active scrobbling		
-			else if @recent.is_a? Array
-				$date1 = @recent[1]["date"]["uts"]
-				if $date1 != $date2
-					begin
-						tweet(@recent[1]["artist"]["content"],@recent[1]["name"])
-						#@log.info ('Tweet sent: ♫ ' + @recent[1]["artist"]["content"] + ' - ' + @recent[1]["name"] + ' #NowPlaying')
-						@log.info("Tweet sent: ♫ #{@recent[1]["artist"]["content"]} - #{@recent[1]["name"]} #NowPlaying")
-						$date2 = $date1
-					end
-			#exception logging		
-				else 	
-					@log.warn 'Unsupported API-Answer'
-				end
-			end
-		end
-	end
+    while 1 != 2 do
+        begin           
+            scrobbleread
+            #tweet if no active scrobbling
+            if @recent.is_a? Hash
+                $date1 = @recent["date"]["uts"]
+                if $date1 != $date2
+                    begin
+                        tweet(@recent["artist"]["content"],@recent["name"])
+                        $date2 = $date1
+                    end
+                end 
+            #tweet if active scrobbling     
+            else if @recent.is_a? Array
+              $date1 = @recent[1]["date"]["uts"]
+              if $date1 != $date2
+                begin
+                  tweet(@recent[1]["artist"]["content"],@recent[1]["name"])
+                  $date2 = $date1
+                end     
+              else    
+                #exception because of who the fuck knows
+                puts 'Unsupported API-Answer'
+              end
+            end
+        end
+    end
 end
 end
